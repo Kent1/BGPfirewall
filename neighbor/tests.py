a534719dcad42a68bfea85154c16733a8e1becdd
Quentin Loos <contact@quentinloos.be>
2013-10-10 11:17:32 +0200

    Add unit tests for Neighbor.

--- neighbor/tests.py
+++ neighbor/tests.py
@@ -1,16 +1,44 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+Unit tests for neighbor application.
 
-Replace this with more appropriate tests for your application.
+Author: Quentin Loos <contact@quentinloos.be>
 """
 
 from django.test import TestCase
 
+from models import Neighbor
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+
+class NeighborTest(TestCase):
+
+    def setUp(self):
+        self.neighbor = Neighbor()
+        self.neighbor.name = 'test'
+        self.neighbor.description = 'description test'
+        self.neighbor.router_id = '10.0.0.1'
+        self.neighbor.local_address = '10.0.0.1'
+        self.neighbor.peer_address = '10.0.0.2'
+        self.neighbor.local_as = 65000
+        self.neighbor.peer_as = 65100
+        self.neighbor.enable = True
+        self.neighbor.save()
+
+    def test_enable(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test if the enable field and method connect and disconnect are correct.
         """
-        self.assertEqual(1 + 1, 2)
+
+        self.assertEqual(self.neighbor.enable_changed(), False)
+        self.neighbor.enable = True
+        self.assertEqual(self.neighbor.enable_changed(), False)
+        self.neighbor.enable = False
+        self.assertEqual(self.neighbor.enable_changed(), True)
+
+        self.neighbor.disconnect()
+        self.assertEqual(self.neighbor.enable, False)
+        self.neighbor.disconnect()
+        self.assertEqual(self.neighbor.enable, False)
+
+        self.neighbor.save()
+        self.neighbor.connect()
+        self.assertEqual(self.neighbor.enable_changed(), True)
