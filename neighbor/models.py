1ff9603eaab6be308bac0aa8623d9398c6d33f99
Quentin Loos <contact@quentinloos.be>
2013-10-10 10:43:08 +0200

    Add methods to connect and disconnect neighbor and set enable field.

--- neighbor/models.py
+++ neighbor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Neighbor(models.Model):
 
     """This class represents a neighbor in BGP."""
@@ -20,5 +21,41 @@ class Neighbor(models.Model):
 
     enable        = models.BooleanField(default=False)
 
+    def connect(self):
+        """Enable BGP connection with the neighbor. Add neighbor to ExaBGP."""
+        self.enable = True
+
+    def disconnect(self):
+        """Disconnect the neighbor."""
+        self.enable = False
+
+    def enable_changed(self):
+        """Return True if enable has changed."""
+        if self.pk is not None:
+            orig = Neighbor.objects.get(pk=self.pk)
+            if orig.enable != self.enable:
+                # If enable has changed
+                return True
+        return False
+
+    def save(self, *args, **kwargs):
+        if self.enable_changed():
+            if self.enable:
+                # Try to connect with the neighbor
+                self.connect()
+            else:
+                self.disconnect()
+        super(Neighbor, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if not self.enable:
+            # Try to disconnect with the neighbor
+            pass
+        super(Neighbor, self).delete(*args, **kwargs)
+
     def __unicode__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "Neighbor"
+        verbose_name_plural = "Neighbors"
