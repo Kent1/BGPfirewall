d1f37327702fbc404b6388c4f0a66fee18e251a0
Quentin Loos <contact@quentinloos.be>
2013-10-17 14:29:42 +0200

    Modify admin forms of flows

--- flow/admin.py
+++ flow/admin.py
@@ -1,14 +1,56 @@
-from django.db.models import get_models, get_app
 from django.contrib import admin
-from django.contrib.admin.sites import AlreadyRegistered
+from flow.models import *
 
-def autoregister(*app_list):
-    for app_name in app_list:
-        app_models = get_app(app_name)
-        for model in get_models(app_models):
-            try:
-                admin.site.register(model)
-            except AlreadyRegistered:
-                pass
 
-autoregister('flow')
+class ProtocolInLine(admin.StackedInline):
+    model = Protocol
+    extra = 1
+
+
+class PortInLine(admin.StackedInline):
+    model = Port
+    extra = 1
+
+
+class PacketLengthInLine(admin.StackedInline):
+    model = PacketLength
+    extra = 1
+
+
+class DSCPInLine(admin.StackedInline):
+    model = DSCP
+    extra = 1
+
+
+class ICMPInLine(admin.StackedInline):
+    model = ICMP
+    extra = 1
+
+
+class TCPFlagInLine(admin.StackedInline):
+    model = TCPFlag
+    extra = 1
+
+
+class FragmentInLine(admin.StackedInline):
+    model = Fragment
+    extra = 1
+
+
+class MatchAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('IP addresses', {'fields': ['destination', 'source']}),
+    ]
+    inlines = [ProtocolInLine, PortInLine, PacketLengthInLine, DSCPInLine, ICMPInLine, TCPFlagInLine, FragmentInLine]
+
+
+class FlowAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'status')
+    fieldsets = [
+        ('Route',  {'fields': ['name', 'description', 'expires']}),
+        ('Match',  {'fields': ['match']}),
+        ('Then',   {'fields': ['then', 'then_value']}),
+    ]
+
+admin.site.register(Flow, FlowAdmin)
+admin.site.register(Match, MatchAdmin)
