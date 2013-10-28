fc9b4e6979524e4376f7c3893eb2316e2de927fa
Quentin Loos <contact@quentinloos.be>
2013-10-28 13:56:43 +0100

    Use Admin site to build our site

--- flow/admin.py
+++ flow/admin.py
@@ -1,16 +1,56 @@
 from django.contrib import admin
 from flow.models import *
-from flow.forms import FlowForm, MatchForm
 
+
+class ProtocolInline(admin.StackedInline):
+
+    model = Protocol
+    extra = 1
+
+
+class PortInline(admin.StackedInline):
+
+    model = Port
+    extra = 1
+
+
+class PacketLengthInline(admin.StackedInline):
+
+    model = PacketLength
+    extra = 1
+
+
+class DSCPInline(admin.StackedInline):
+
+    model = DSCP
+    extra = 1
+
+
+class ICMPInline(admin.StackedInline):
+
+    model = ICMP
+    extra = 1
+
+
+class TCPFlagInline(admin.StackedInline):
+
+    model = TCPFlag
+    extra = 1
+
+
+class FragmentInline(admin.StackedInline):
+
+    model = Fragment
+    extra = 1
 
 class FlowAdmin(admin.ModelAdmin):
-    form = FlowForm
+
     list_display = ('name', 'description', 'status')
     fieldsets = [
         ('Route',  {'fields': ['name', 'description', 'expires', 'active']}),
-        ('Match',  {'fields': ['source', 'destination', 'protocol', ('port_number', 'direction')]}),
-        ('Advanced Match', {'classes': ['collapse'], 'fields' : ['packet_length', 'dscp', ('icmp_type', 'icmp_code'), 'tcp_flag', 'fragment']}),
+        ('Match',  {'fields': ['source', 'destination']}),
         ('Then',   {'fields': ['then', 'then_value']}),
     ]
+    inlines = [ProtocolInline, PortInline, PacketLengthInline, DSCPInline, ICMPInline, TCPFlagInline, FragmentInline]
 
 admin.site.register(Flow, FlowAdmin)
