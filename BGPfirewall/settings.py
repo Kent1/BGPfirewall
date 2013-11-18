dc70c91a8b7d6ab0f751a7f17700d3f68b7f2906
Quentin Loos <contact@quentinloos.be>
2013-11-18 11:32:07 +0100

    Modify admin templates.

--- BGPfirewall/settings.py
+++ BGPfirewall/settings.py
@@ -1,4 +1,5 @@
 # Django settings for BGPfirewall project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -108,10 +109,14 @@ ROOT_URLCONF = 'BGPfirewall.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'BGPfirewall.wsgi.application'
 
+#Base Directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR,'..', 'templates'),
 )
 
 INSTALLED_APPS = (
